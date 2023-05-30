82f540937ddc76fcb77e76efe6970e8850603a53
yiwong <yiwong@student.42wolfsburg.de>
2023-05-30 18:49:51 +0200

    pixel tests working

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 15:55:54 by yiwong            #+#    #+#             */
-/*   Updated: 2023/05/30 17:17:25 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/05/30 18:35:29 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ typedef struct	s_data {
 	int		line_length;
 	int		endian;
 }				t_data;
+
+#define colour1 0x000000
+#define colour2 0xFFFFFF
+
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 
 
@@ -32,9 +37,16 @@ int	main(void)
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "It's gamer time");
 	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-		&img.endian);
-	my_mlx_pixel_put(&img, 0, 0, 0x00FFFF00);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+		&img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 0, 0, colour2);
+	my_mlx_pixel_put(&img, 0, 1, colour2);
+	my_mlx_pixel_put(&img, 0, 2, colour2);
+	my_mlx_pixel_put(&img, 1, 2, colour2);
+	my_mlx_pixel_put(&img, 2, 2, colour2);
+	my_mlx_pixel_put(&img, 2, 1, colour2);
+	my_mlx_pixel_put(&img, 2, 0, colour2);
+	my_mlx_pixel_put(&img, 1, 0, colour2);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 10, 10);
 	mlx_loop(mlx);
 	return (0);
