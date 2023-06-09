a8cf7d7a7d3b965281aa0bc27386dbce96bc0a0d
yiwong <yiwong@student.42wolfsburg.de>
2023-06-09 18:24:26 +0200

    adds printing error messages to stderr

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 17:20:35 by yiwong            #+#    #+#             */
-/*   Updated: 2023/06/07 20:20:17 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/06/08 16:03:18 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ void error(int error_code)
 		write(2, "Error\nInvalid file name argument\n", 23);
 	else if (error_code == BAD_MAP)
 		write(2, "Error\nInvalid map\n", 18);
-	else if (error_code == NOT_RECTANGLE)
-		write(2, "Error\nMap is not rectangular\n", 29);
 	else
 		write(2, "Error\nUndefined\n", 16);
 	write(2, RESET, 7);
