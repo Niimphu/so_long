568374e5cff46d942a523aeee427d88c645002d0
yiwong <yiwong@student.42wolfsburg.de>
2023-06-13 19:27:16 +0200

    window closed when esc pressed

--- src/input_handler.c
+++ src/input_handler.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 15:30:35 by yiwong            #+#    #+#             */
-/*   Updated: 2023/06/12 16:12:18 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/06/13 19:16:16 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ int	key_pressed(int keycode, t_vars *vars)
 {
 	ft_printf("key pressed: %i\n", keycode);
 	if (keycode == XK_Escape)
+	{
+		mlx_clear_window(vars->mlx, vars->win);
+		mlx_destroy_window(vars->mlx, vars->win);
 		quit(vars);
+	}
 	return (0);
 }
