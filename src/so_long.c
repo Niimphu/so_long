5a53e6b314d794cb3d443bf0a393b8d2e9dedab2
yiwong <yiwong@student.42wolfsburg.de>
2023-06-15 18:53:22 +0200

    .

--- src/so_long.c
+++ src/so_long.c
@@ -6,18 +6,22 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 15:16:58 by yiwong            #+#    #+#             */
-/*   Updated: 2023/06/14 19:11:17 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/06/15 18:52:04 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../dep/so_long.h"
 
-#define PLAYER "../sprites/Player.png"
+#define PLAYER "../sprites/Player.xpm"
+#define BG "../sprites/Background.xpm"
+
+// int	draw_something(t_vars *vars);
 
 int	so_long(t_vars *vars)
 {
 	mlx_hook(vars->win, KeyPress, 1L << 0, key_pressed, vars);
 	mlx_hook(vars->win, DestroyNotify, 0L, window_closed, vars);
+	draw_something(vars);
 	mlx_loop(vars->mlx);
 	return (0);
 }
@@ -25,6 +29,10 @@ int	so_long(t_vars *vars)
 // int	draw_something(t_vars *vars)
 // {
 // 	void	*img;
+// 	int		img_width = 64;
+// 	int		img_height = 64;
 
-// 	img = mlx_png_file_to_image(mlx, PLAYER, &img_width, &img_height);
+// 	img = mlx_xpm_file_to_image(vars->mlx, PLAYER, &img_width, &img_height);
+// 	mlx_put_image_to_window(vars->mlx, vars->win, img->img, 0, 0);
+// 	return (0);
 // }
