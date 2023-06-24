3d99689d5bd1dbdf6bbbc5ed5918cfe2fc7ac784
yiwong <yiwong@student.42wolfsburg.de>
2023-06-24 18:33:24 +0200

    added ability to take arguments either as path or as name of map in maps/ directory

--- src/read_map.c
+++ src/read_map.c
@@ -12,21 +12,40 @@
 
 #include "../dep/so_long.h"
 
+char	*find_map(char *name);
 int	get_map_height(int *fd, char *name);
 
 int	open_map_file(char *name)
 {
 	int		fd;
-	char	*temp;
+	char	*path_to_map;
 
-	temp = ft_strjoin(name, ".ber");
-	name = ft_strjoin("maps/", temp);
-	free_pointer(temp);
-	fd = open(name, O_RDONLY, 0666);
-	free_pointer(name);
+	path_to_map = find_map(name);
+	if (!path_to_map)
+		fd = open(name, O_RDONLY, 0666);
+	else
+	{
+		fd = open(path_to_map, O_RDONLY, 0666);
+		free_pointer(path_to_map);
+	}
 	if (fd < 0)
 		return (0);
 	return (fd);
+}
+
+char	*find_map(char *name)
+{
+	char	*temp;
+
+	if (ft_strnstr(name, ".ber", ft_strlen(name)) || ft_strrchr(name, '/'))
+		return (NULL);
+	else
+	{
+		temp = ft_strjoin(name, ".ber");
+		name = ft_strjoin("maps/", temp);
+		free_pointer(temp);
+	}
+	return (name);
 }
 
 char	**read_map(int fd, char *name)
