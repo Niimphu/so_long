b6b2322a35e527bbcfd613a88853c1a66ab1d54a
yiwong <yiwong@student.42wolfsburg.de>
2023-06-25 17:17:38 +0200

    new bonus files

---
+++ src/bonus/so_long_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 14:22:17 by yiwong            #+#    #+#             */
+/*   Updated: 2023/06/25 15:24:23 by yiwong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../dep/so_long_bonus.h"
+
+int	so_long(t_vars *vars)
+{
+	mlx_loop_hook(vars->mlx, loop, vars);
+	mlx_hook(vars->win, KeyPress, 1L << 0, key_pressed, vars);
+	mlx_hook(vars->win, DestroyNotify, 0L, window_closed, vars);
+	draw(vars);
+	update_move_counter(vars);
+	mlx_loop(vars->mlx);
+	return (0);
+}
---
+++ src/bonus/time_loop.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_loop.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 14:21:36 by yiwong            #+#    #+#             */
+/*   Updated: 2023/06/25 14:21:36 by yiwong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../dep/so_long_bonus.h"
+
+void	update_frame(t_vars *vars);
+
+int	loop(t_vars *vars)
+{
+	clock_t					time_since_start;
+	static unsigned long	iterations;
+
+	if (!iterations)
+		iterations = 0;
+	time_since_start = clock() / 120000;
+	if (time_since_start > iterations)
+	{
+		iterations++;
+		update_frame(vars);
+	}
+	return (0);
+}
+
+void	update_frame(t_vars *vars)
+{
+	vars->frame++;
+	if (vars->frame > 7)
+		vars->frame = 0;
+	draw(vars);
+}
